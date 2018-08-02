3ddcec9fbca3763864bd0c87a897dfb6809e7cd6
songxingguo <1328989942@qq.com>
2018-08-03 01:53:43 +0800

    修改index

--- routes/index.js
+++ routes/index.js
@@ -13,14 +13,21 @@ router.get('/', async (ctx, next) => {
   })
 })
 
-// router.get('/string', async (ctx, next) => {
-//   ctx.body = 'koa2 string'
-// })
-//
-// router.get('/json', async (ctx, next) => {
-//   ctx.body = {
-//     title: 'koa2 json'
-//   }
-// })
+router.get('/string', async (ctx, next) => {
+  ctx.body = 'koa2 string'
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json',
+    works: [{
+      name: "网站建设",
+      content:"专业建站团队为您售后服务"
+    },{
+      name: "虚拟主机",
+      content:"快速稳定虚拟机空间出租"
+    }]
+  }
+})
 
 module.exports = router
