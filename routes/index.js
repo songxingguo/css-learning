3b7d989df4f1b572fffa847c57fd9fa2ba7a920a
songxingguo <1328989942@qq.com>
2018-08-03 01:04:42 +0800

    完成

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,25 @@ const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: '作品展示'
+    title: '作品展示',
+    works: [{
+     name: "网站建设",
+     content:"专业建站团队为您售后服务"
+    },{
+      name: "虚拟主机",
+      content:"快速稳定虚拟机空间出租"
+    }]
   })
 })
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
-router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
-})
+// router.get('/string', async (ctx, next) => {
+//   ctx.body = 'koa2 string'
+// })
+//
+// router.get('/json', async (ctx, next) => {
+//   ctx.body = {
+//     title: 'koa2 json'
+//   }
+// })
 
 module.exports = router
