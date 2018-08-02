cd792ad95995c0e0869bb88eb0cb0d99c8a4b589
songxingguo <1328989942@qq.com>
2018-08-02 18:21:41 +0800

    app.js

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
+const serve = require('koa-static');
 const Koa = require('koa');
 const app = new Koa();
-const fs = require('fs');
-
-app.use(async ctx => {
-    ctx.response.type = 'html';
-    ctx.response.body = fs.createReadStream('./index.html');
-});
-
+ 
+// $ GET /package.json
+app.use(serve('.'));
+ 
 app.listen(3000);
-
-// 终端打印如下信息
-console.log('Server running at http://127.0.0.1:3000/');
+ 
+console.log('listening on port 3000');
